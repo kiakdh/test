4f69a95b4946b7c2161d5c577b387e25e28edb3a
kiakdh <kiakdh93@gmail.com>
2021-10-01 23:47:22 +0000

    dfag

---
+++ asdfasdf.py
@@ -0,0 +1,22 @@
+def mul(A,B):
+    cnt = 0
+    K=0
+    while True:
+        cnt += 1
+        K += cnt*B
+        if cnt*B>A:
+            K -= cnt*B
+            break
+    return(K)
+
+
+
+
+K = input("정수를 입력해주세요: ")
+N = input("원하는 배수1을 입력해주세요: ")
+M = input("원하는 배수2를 입력해주세요: ")
+
+X = mul(K,N)
+print(X)
+
+
