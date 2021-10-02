130cdee0a6bbd711a8e1d2c3245a10addc242970
kiakdh <kiakdh93@gmail.com>
2021-10-02 03:53:08 +0000

    ㄴㅇㄹ

---
+++ 과제/다익스트라 알고리즘.py
@@ -0,0 +1,9 @@
+
+
+
+
+
+N = int(input(정점의 수를 입력해주세요: ))
+E = int(input(간선의 수를 입력해주세요: ))
+
+for i in range(E):
---
+++ 과제/연습장.py
@@ -0,0 +1,7 @@
+N = int(input(정점:))
+L = []
+for i in range(N):
+    K = int(input())
+    L.append(K)
+
+print(L)
