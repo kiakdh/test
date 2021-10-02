ae24aabbe319a292e0cff1b9d3c488611ef8704c
kiakdh <kiakdh93@gmail.com>
2021-10-02 04:02:53 +0000

    dafs

--- 과제/다익스트라 알고리즘.py
+++ 과제/다익스트라 알고리즘.py
@@ -3,7 +3,8 @@
 
 
 
-N = int(input(정점의 수를 입력해주세요: ))
-E = int(input(간선의 수를 입력해주세요: ))
+N = int(input("정점의 수를 입력해주세요: "))
+E = int(input("간선의 수를 입력해주세요: "))
 
 for i in range(E):
+    A1 = r
--- 과제/연습장.py
+++ 과제/연습장.py
@@ -1,7 +1,8 @@
-N = int(input(정점:))
+N = int(input("ㅎㅇ:"))
 L = []
-for i in range(N):
-    K = int(input())
-    L.append(K)
 
-print(L)
+for i in range(N):
+    A1 = input("간선%d의 정보1를 입력해주세요: "%i)
+    A2 = input("간선%d의 정보2를 입력해주세요: "%i)
+    L.append([A1,A2])
+    print(L)
